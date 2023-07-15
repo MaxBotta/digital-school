d3ee08f34d27906a3bcb6f8fc6487133d7fce43b
Maximilian Botta <max.botta@gmx.de>
2023-07-15 14:17:44 +0200

    Sa 10 & 12:30 added sprite animations for player

--- SA10/MeineGamesWebsite/testGame.js
+++ SA10/MeineGamesWebsite/testGame.js
@@ -1,6 +1,5 @@
 
-let sprite1;
-let sprite2;
+let player;
 
 let floor;
 let canvas;
@@ -15,28 +14,18 @@ function setup() {
     canvas.parent("game");
     world.gravity.y = 10;
 
-    // sprite1 = new Sprite(0, 200, 50, 50);
-    // sprite1.color = "yellow";
-    // sprite1.vel.x = 6;
+    player = new Sprite(500, 300, 50, 50, "kinematic");
+    player.addAni("run", "./assets/tiny-hero-sprites/Pink_Monster/Pink_Monster_Run_6.png", {frameSize: [32, 32], frameCount: 6});
+    player.addAni("standing", "./assets/tiny-hero-sprites/Pink_Monster/Pink_Monster.png", {frameSize: [32, 32], frameCount: 1});
+    player.ani.frameDelay = 3;
 
-    // sprite2 = new Sprite(600, 200, 50, 50);
-    // sprite2.color = "yellow";
-    // sprite2.vel.x = -6;
-    // sprite2.rotation = 30;
-
-    floor = new Sprite(300, 400, 600, 20, "static");
+    floor = new Sprite(300, 380, 600, 40, "static");
     floor.color = "green";
 
-    for (let e = 6; e > 0; e--) {
-        for (let i = 0; i < e; i++) {
-            let newSprite = new Sprite(100, 360 - 68/e, 20, 20);
-            newSprite.color = "yellow";
-            // newSprite.diameter = 20;
-            // newSprite.vel.x = random(-6, 6);
-            // newSprite.vel.y = random(1, 6);
-            // newSprite.img = "./assets/flappy2-32x24.png";
-            // newSprite.ani.frameDelay = 3;
-        }
+    
+    for(let y = 0; y < 5; y++) {
+        let newSprite = new Sprite(100, 348 - y * 24 , 32, 24);
+        newSprite.addAni("flappy", "./assets/flappy-sheet.png", {frameSize: [32, 24], frameCount: 4} )
     }
 
 
@@ -46,4 +35,24 @@ function draw() {
     clear();
     background(0, 150, 255);
 
+    //Steuerung
+    if(kb.pressing("left")) {
+        player.vel.x = -10;
+        player.ani = "run";
+        player.mirror.x = true;
+    } else if(kb.pressing("right")) {
+        player.vel.x = 10;
+        player.ani = "run";
+        player.mirror.x = false;
+    } else {
+        player.vel.x = 0;
+        player.ani = "standing";
+    }
+
+    if(kb.pressed("up")) {
+        player.vel.y = 20;
+    }
+
 }
+
+
