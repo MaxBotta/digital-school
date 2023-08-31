24dfcbca0e6598cdbfe9b6f4d5607290d05a4c33
Maximilian Botta <max.botta@gmx.de>
2023-08-31 18:22:57 +0100

    Do1715 sprite animation

---
+++ DO1715/TestProjekt/game.js
@@ -0,0 +1,55 @@
+
+let player;
+
+function setup() {
+  const canvas = new Canvas(1000, 600);
+  canvas.parent("game");
+
+  player = new Sprite();
+  player.x = 200;
+  player.y = 200;
+  player.width = 40;
+  player.height = 40;
+  player.color = "yellow";
+  player.scale = 2;
+  // player.addAni('idle', 'assets/PirateBomb/Sprites/1-Player-Bomb Guy/2-Run/1.png', 14);
+  player.addAni('idle', 'assets/wind-hashashin/PNG/idle/idle_1.png', 8);
+  player.addAni('run', 'assets/wind-hashashin/PNG/run/run_1.png', 8);
+  player.addAni('atk-1', 'assets/wind-hashashin/PNG/1_atk/1_atk_1.png', 8);
+  player.addAni('atk-2', 'assets/wind-hashashin/PNG/2_atk/2_atk_1.png', 18);
+
+  player.ani = "idle";
+
+
+
+}
+
+function draw() {
+  background(0, 150, 250);
+
+  //Spieler nach links und rechts bewegen
+  if (kb.pressing('left')) {
+    player.mirror.x = true;
+    player.vel.x = -8;
+    player.changeAni("run");
+  } else if (kb.pressing('right')) {
+    player.mirror.x = false;
+    player.vel.x = 8;
+    player.changeAni("run");
+  } else {
+    player.vel.x = 0;
+
+    //Spieler attacke
+    if (kb.pressing("f")) {
+      player.changeAni("atk-1");
+    } else if (kb.pressing("g")) {
+      player.changeAni("atk-2");
+    } else {
+      player.changeAni("idle");
+    }
+
+  }
+
+
+}
+
