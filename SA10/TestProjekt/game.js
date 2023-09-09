33ddee6b4a2f53b2e4aa8a5a30120a7beec314b6
Maximilian Botta <max.botta@gmx.de>
2023-09-09 10:35:37 +0100

    Sa10 added jump animation and platform

--- SA10/TestProjekt/game.js
+++ SA10/TestProjekt/game.js
@@ -1,22 +1,46 @@
 
 let player;
+let ground;
+let platform1;
 
 
 function setup() {
   const canvas = new Canvas(1000, 600);
   canvas.parent("game");
 
+  world.gravity.y = 10;
+
+  ground = new Sprite();
+  ground.x = 500;
+  ground.y = 580;
+  ground.width = 1000;
+  ground.height = 40;
+  ground.color = "green";
+  ground.collider = "static";
+
+  platform1 = new Sprite();
+  platform1.x = 600;
+  platform1.y = 400;
+  platform1.width = 200;
+  platform1.height = 40;
+  platform1.color = "green";
+  platform1.collider = "static";
+
   player = new Sprite();
   player.x = 200;
   player.y = 200;
-  player.width = 40;
-  player.height = 60;
+  player.width = 22;
+  player.height = 32;
   player.color = "yellow";
   player.scale = 2;
+  player.debug = true;
+  player.isJumping = false;
 
   player.addAni("run", "assets/tiny-hero-sprites/1 Pink_Monster/Pink_Monster_Run_6.png", { frameSize: [32, 32], frames: 6 })
   player.addAni("idle", "assets/tiny-hero-sprites/1 Pink_Monster/Pink_Monster_Idle_4.png", { frameSize: [32, 32], frames: 4 })
+  player.addAni("jump", "assets/tiny-hero-sprites/1 Pink_Monster/Pink_Monster_Jump_8.png", { frameSize: [32, 32], frames: 8 })
 
+  player.changeAni("idle");
   player.ani.frameDelay = 8;
 
 }
@@ -24,18 +48,40 @@ function setup() {
 function draw() {
   background(0, 150, 250);
 
-
-  if (kb.pressing('left')) {
-    player.changeAni('run');
-    player.vel.x = -6;
-    player.mirror.x = true;
-  } else if (kb.pressing('right')) {
-    player.changeAni('run');
-    player.vel.x = 6;
-    player.mirror.x = false;
-  } else {
-    player.changeAni('idle');
-    player.vel.x = 0;
+  //Wenn Spieler hoch Taste drückt, dann springt Spieler
+  if (kb.presses('up')) {
+    player.isJumping = true;
+    player.vel.y = 40;
+    player.changeAni(["jump", "idle"]);
   }
 
+  if(player.vel.y === 0) {
+    player.isJumping = false;
+  }
+
+  //Wenn Spieler nicht springt, dann können wir laufen
+  if (player.isJumping === false) {
+
+    if (kb.pressing('left')) {
+      player.changeAni('run');
+      player.vel.x = -6;
+      player.mirror.x = true;
+    } else if (kb.pressing('right')) {
+      player.changeAni('run');
+      player.vel.x = 6;
+      player.mirror.x = false;
+    } else {
+      player.changeAni('idle');
+      player.vel.x = 0;
+    }
+
+  }
+
+
 }
+
+
+
+
+
+
