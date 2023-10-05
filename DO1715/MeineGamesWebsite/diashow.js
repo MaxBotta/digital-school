a4c9dc0daf0761cd54fe6f605862570d0cf09b2e
Maximilian Botta <max.botta@gmx.de>
2023-10-05 18:31:44 +0100

    Do1715 Added Diashow

---
+++ DO1715/MeineGamesWebsite/diashow.js
@@ -0,0 +1,49 @@
+
+
+//Hole Referenz auf Diashow Element aus dem HTML
+const diashowElmt = document.getElementById("diashow-1");
+
+// animateDiashow(diashowElmt);
+
+
+function animateDiashow(diashowElmt) {
+
+    //Wie viele Bilder sind in der Diashow?
+    //Hole alle Bilder aus der Diashow
+    const images = diashowElmt.getElementsByTagName("img");
+
+    //Blende das erste Bild ein
+    images[0].style.opacity = 1;
+
+    //Erstelle einen Counter der das aktuelle angezeigte Bild speichert
+    let counter = 0;
+
+    //Erstelle ein Interval, dass alle zwei Sekunden das nöchste Bild anzeigt
+    setInterval(function () {
+        console.log(counter)
+
+        //Blende letztes Bild wieder aus
+        images[counter].style.opacity = 0;
+
+        //Erhöhe den COunter um eins
+        counter++;
+
+        //Wenn Counter bei Anzahl von Bildern angekommen ist, dann setze COunter auf 0
+        if (counter === images.length) {
+            counter = 0;
+        }
+
+        //Blende nächstes Bild ein
+        images[counter].style.opacity = 1;
+    }, 2000);
+
+}
+
+
+
+let counter = 0;
+
+setInterval(function() {
+    counter++;
+    console.log(counter);
+}, 1000);
