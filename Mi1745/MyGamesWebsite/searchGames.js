b6a770938c1fc268fc62b6a5a868ae643831d40f
Maximilian Botta <max.botta@gmx.de>
2023-11-02 17:54:44 +0100

    Do1715 Diashow Buttons

--- Mi1745/MyGamesWebsite/searchGames.js
+++ Mi1745/MyGamesWebsite/searchGames.js
@@ -6,3 +6,15 @@
  * - Durchsuche alle Game Thumbnail Titel und schaue ob Suchtext mit Titel übereinstimmt
  * - Blende alle Elmente aus, die nicht übereinstimmen mit dem Suchtext
  */
+
+
+
+const searchInput = document.getElementById("search-games");
+
+
+searchInput.addEventListener("keyup", filterGames);
+
+function filterGames() {
+    const searchValue = searchInput.value;
+    console.log(searchValue);
+}
