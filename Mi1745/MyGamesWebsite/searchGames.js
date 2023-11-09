96887fe85cb93e3cd4c27f248a51503c35e92605
Maximilian Botta <max.botta@gmx.de>
2023-11-09 17:55:38 +0100

    Do1745 search

--- Mi1745/MyGamesWebsite/searchGames.js
+++ Mi1745/MyGamesWebsite/searchGames.js
@@ -14,20 +14,36 @@ const searchInput = document.querySelector("#js-search-games");
 
 //Füge ein keyup Event hinzu
 //Wird immer ausgelöst, wenn Nutzer tippt
-searchInput.addEventListener("keyup", filterGames);
+searchInput.addEventListener("keyup", filterBySearch);
 
-function filterGames() {
+function filterBySearch() {
     const searchValue = searchInput.value.toLowerCase();
 
     //Durchsuche jedes Game und gleiche den Titel mit dem Suchwert ab
-    for(let game of games) {
+    for (let game of games) {
         const title = game.querySelector("h5").innerText.toLowerCase()
-        
-        if(title.includes(searchValue) === false) {
+
+        if (title.includes(searchValue) === false) {
             game.style.display = "none";
         } else {
             game.style.display = "block";
         }
     }
+    
+}
 
+function filterByTag(tagname) {
+    console.log("🚀 ~ file: searchGames.js:36 ~ filterByTag ~ tagname:", tagname)
+    for (let game of games) {
+        const tags = game.querySelectorAll(".tag");
+        console.log("🚀 ~ file: searchGames.js:39 ~ filterByTag ~ tags:", tags)
+
+        for(let tag of tags) {
+            if (tag.innerText == tagname) {
+                game.style.display = "none";
+            } else {
+                game.style.display = "block";
+            }
+        }
+    }
 }
