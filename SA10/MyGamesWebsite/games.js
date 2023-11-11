f58cbc3fd4736f823f35fcd7e1c63bd80bfb3ab4
Maximilian Botta <max.botta@gmx.de>
2023-11-11 10:09:15 +0100

    Sa10 added games.html and js

---
+++ SA10/MyGamesWebsite/games.js
@@ -0,0 +1,10 @@
+
+
+/**
+ * To Do:
+ * - Alles Karten/Spiele suchen
+ * - Wörter/Buchstaben aus Suchleiste auslesen
+ * - Spiele nach begriffen filtern / ein und ausblenden 
+ * - Immer wenn in der Suchleiste getippt wird, wieder Spiele filtern / Keyup Event für die Suchleiste anlegen 
+ */
+
