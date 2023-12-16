e54cde04656f49bcf11fe93bc2a15569f72a06b1
Maximilian Botta <max.botta@gmx.de>
2023-12-16 11:24:12 +0100

    Sa10 added all player spritesheets

--- SA10/NinjaParkour/scenes/Preload.js
+++ SA10/NinjaParkour/scenes/Preload.js
@@ -15,9 +15,26 @@ export class Preload extends Phaser.Scene {
             "Pink Man",
             "Virtual Guy",
         ]
+
     }
 
     preload = () => {
+        var progressBar = this.add.graphics();
+        var progressBox = this.add.graphics();
+        progressBox.fillStyle(0x222222, 0.8);
+        progressBox.fillRect(240, 270, 320, 50);
+
+        this.load.on('progress', function (value) {
+            console.log(value);
+            progressBar.clear();
+            progressBar.fillStyle(0xffffff, 1);
+            progressBar.fillRect(250, 280, 300 * value, 30);
+        });
+
+        this.load.on('complete', function () {
+            console.log('complete');
+        });
+
         this.loadPlayerAnimations();
     }
 
@@ -26,10 +43,70 @@ export class Preload extends Phaser.Scene {
     }
 
     loadPlayerAnimations = () => {
-        for(let type of this.characterNames) {
+        for (let type of this.characterNames) {
             this.load.spritesheet({
                 key: `${type}_idle`,
                 url: `assets/PixelAdventure/MainCharacters/${type}/Idle (32x32).png`,
+                frameConfig: {
+                    frameWidth: 32,
+                    frameHeight: 32,
+                    startFrame: 0,
+                    endFrame: 11
+                }
+            });
+            this.load.spritesheet({
+                key: `${type}_fall`,
+                url: `assets/PixelAdventure/MainCharacters/${type}/Fall (32x32).png`,
+                frameConfig: {
+                    frameWidth: 32,
+                    frameHeight: 32,
+                    startFrame: 0,
+                    endFrame: 11
+                }
+            });
+            this.load.spritesheet({
+                key: `${type}_hit`,
+                url: `assets/PixelAdventure/MainCharacters/${type}/Hit (32x32).png`,
+                frameConfig: {
+                    frameWidth: 32,
+                    frameHeight: 32,
+                    startFrame: 0,
+                    endFrame: 11
+                }
+            });
+            this.load.spritesheet({
+                key: `${type}_double_jump`,
+                url: `assets/PixelAdventure/MainCharacters/${type}/Double Jump (32x32).png`,
+                frameConfig: {
+                    frameWidth: 32,
+                    frameHeight: 32,
+                    startFrame: 0,
+                    endFrame: 11
+                }
+            });
+            this.load.spritesheet({
+                key: `${type}_jump`,
+                url: `assets/PixelAdventure/MainCharacters/${type}/Jump (32x32).png`,
+                frameConfig: {
+                    frameWidth: 32,
+                    frameHeight: 32,
+                    startFrame: 0,
+                    endFrame: 11
+                }
+            });
+            this.load.spritesheet({
+                key: `${type}_wall_jump`,
+                url: `assets/PixelAdventure/MainCharacters/${type}/Wall Jump (32x32).png`,
+                frameConfig: {
+                    frameWidth: 32,
+                    frameHeight: 32,
+                    startFrame: 0,
+                    endFrame: 11
+                }
+            });
+            this.load.spritesheet({
+                key: `${type}_run`,
+                url: `assets/PixelAdventure/MainCharacters/${type}/Run (32x32).png`,
                 frameConfig: {
                     frameWidth: 32,
                     frameHeight: 32,
