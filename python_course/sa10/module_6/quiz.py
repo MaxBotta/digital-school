2553fddabeb015bbda3f4a25bb329f7f3bf28ea7
Maximilian Botta <max.botta@gmx.de>
2024-11-02 10:45:50 +0100

    sa10 quiz game

--- python_course/sa10/module_6/quiz.py
+++ python_course/sa10/module_6/quiz.py
@@ -5,8 +5,16 @@ At the end of the quiz, the player’s final score will be shown.
 For this app we will use variables, functions, if statements and loops.
 '''
 
-from random import randint
+'''
+To Dos:
+- Das Programm soll dem Nutzer nacheinander 5 zufällige fragen stellen
+- Für jede Frage hat der Nutzer 3 Chancen
+- Für jede richtig beantwortete Frage erhält der Spieler einen Punkt
+- Wurden alle fragen beantwortet, wird das Programm beendet
+'''
 
+
+from random import randint
 
 questions = [
     {
@@ -31,8 +39,31 @@ questions = [
     }
 ]
 
-# Zugriff auf Werte in List of Dictionaries questions[0]['answer']
+score = 0
 
-random_number = randint(0, len(questions) - 1)
+# Wiederhole x mal: Stelle dem Nutzer eine Frage
+for q in questions:
+    # Stelle dem Nutzer die Frage
+    print(q['question'])
+    
+    # Zähle Antworten
+    answer_counter = 0
+    
+    while answer_counter < 3:
+        player_input = input('Your answer: ')
+        
+        #Kontrolliere ob die Spielerantwort richtig ist
+        if player_input == q['answer']:
+            score += 1
+            print('This answer is right :)')
+            break
+        else:
+            answer_counter += 1
+            print('This answer is wrong :/')
+            
 
-question = questions[random_number]['question']
+
+
+# Gibt eine zufällige ganze Zahl zurück (0 - Anzahl der Fragen)
+#random_number = randint(0, len(questions) - 1)
+
