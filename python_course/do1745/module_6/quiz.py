4d703728797d7f56f3e4fca420373fee87348870
Maximilian Botta <max.botta@gmx.de>
2024-11-14 19:00:13 +0100

    do added tries to quiz game

--- python_course/do1745/module_6/quiz.py
+++ python_course/do1745/module_6/quiz.py
@@ -1,8 +1,10 @@
 '''
 To Dos:
-- Soll dem Nutzer 5 Fragen stellen
-- Soll bei jeder Frage 3 Antwortmöglichkeiten anbieten
-- Wenn die richtige Antwort gegeben wurde, bekommt der Spieler einen Punkt
+-   Soll dem Nutzer 5 Fragen stellen
+-   Soll bei jeder Frage 3 Antwortmöglichkeiten anbieten
+-   Der Spieler hat für jede Frage 2 Versuche
+-   Wird die Frage beim ersten Versuch richtig beantwortet, bekommt der Spieler zwei Punkte, 
+    beim zweiten Versuch einen Punkt und sonst 0 Punkte
 
 Ergänzung:
 - Die Antworten sollen über ein input an das Programm gegeben werden
@@ -31,12 +33,21 @@ for q in categories[int(player_cat) - 1]['questions']:
     print('b: ', q['options']['b'])
     print('c: ', q['options']['c'])
     
-    player_answer = input('Welche Antwort ist richtig (a, b oder c)')
-    
-    if player_answer == q['answer']:
-        score += 1
-        print('Richtiiiig!!!')
-    else:
-        print('falsch!!! :/')
+    tries = 2
+    while tries > 0:
+        player_answer = input('Welche Antwort ist richtig (a, b oder c)')
+            
+        if player_answer == q['answer']:
+            if tries == 2:
+                score += 2
+            else:
+                score += 1
+            print('Richtiiiig!!!')
+            break
+        else:
+            tries -= 1
+            print('falsch :/')
+            if tries == 0:
+                print('Die richtige Antwort ist', q['answer'])
         
 print('Punkte: ', score)
