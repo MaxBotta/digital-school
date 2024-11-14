76df9847ffb02907539ae53d71f3de901dd89620
Maximilian Botta <max.botta@gmx.de>
2024-11-14 20:12:53 +0100

    do password easy or strong

--- python_course/do1745/module_8/password_generator.py
+++ python_course/do1745/module_8/password_generator.py
@@ -12,21 +12,29 @@ import string
 animals = ['Cat', 'Dog', 'Zebra', 'Lion']
 colors = ['Blue', 'Green', 'Red', 'Orange']
 
-def create_password():
+def create_password(level):
     rand_animal = random.choice(animals)
     rand_color = random.choice(colors)
     rand_number = random.randrange(0, 100)
     special_character = random.choice(string.punctuation)
     rand_letter_low = random.choice(string.ascii_lowercase)
     rand_letter = random.choice(string.ascii_uppercase)
-
-    password = rand_animal + rand_color + str(rand_number) + special_character + rand_letter_low + rand_letter
+    
+    password = ''
+    
+    if level == 1:
+        password = rand_animal + rand_color + str(rand_number)
+    elif level == 2:
+        password = rand_animal + rand_color + str(rand_number) + special_character
+    else:
+        password = rand_animal + rand_color + str(rand_number) + special_character + rand_letter_low + rand_letter
+    
     return password
 
 
 password_is_ok = False
 while password_is_ok == False:
-    password = create_password()
+    password = create_password(1)
     print('')
     print(password)
     
