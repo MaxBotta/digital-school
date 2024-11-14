76711a4f6168df22b06e3ccb2a83b882bfaa236a
Maximilian Botta <max.botta@gmx.de>
2024-11-14 20:08:10 +0100

    do password generator

---
+++ python_course/do1745/module_8/password_generator.py
@@ -0,0 +1,37 @@
+'''
+    Erstelle eine Psswortgenerator App, die durch die Kombination von Wörterns aus 
+    zwei oder mehr Listen von Kategorien ein Passwort erstellt. Zusätzlich soll eine 
+    zufällige Nummer zwischen 0 und 100 enthalten sein und ein Sonderzeichen.
+    
+    Beispiel: KatzeBlau42#
+'''
+
+import random
+import string
+
+animals = ['Cat', 'Dog', 'Zebra', 'Lion']
+colors = ['Blue', 'Green', 'Red', 'Orange']
+
+def create_password():
+    rand_animal = random.choice(animals)
+    rand_color = random.choice(colors)
+    rand_number = random.randrange(0, 100)
+    special_character = random.choice(string.punctuation)
+    rand_letter_low = random.choice(string.ascii_lowercase)
+    rand_letter = random.choice(string.ascii_uppercase)
+
+    password = rand_animal + rand_color + str(rand_number) + special_character + rand_letter_low + rand_letter
+    return password
+
+
+password_is_ok = False
+while password_is_ok == False:
+    password = create_password()
+    print('')
+    print(password)
+    
+    user_answer = input('Password ok? y/n : ')
+    
+    if user_answer == 'y':
+        password_is_ok = True
+
