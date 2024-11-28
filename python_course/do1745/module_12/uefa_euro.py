c8b849d714b5e52857b320a9ec1b56690a339361
Maximilian Botta <max.botta@gmx.de>
2024-11-28 18:37:42 +0100

    do1745 uefa app

--- python_course/do1745/module_12/uefa_euro.py
+++ python_course/do1745/module_12/uefa_euro.py
@@ -1,11 +1,21 @@
+from tkinter import Tk, simpledialog, messagebox
 
+root = Tk()
 
-file = open('/Users/maxbotta/Desktop/Berlitz/DigitalSchool/python_course/do1745/module_12/uefa_euro.txt', 'r')
+messagebox.showinfo('Test', 'Hallo Welt')
 
-for line in file:
-    line = line.rstrip('\n')
-    seperated = line.split('/')
-    print('year: ', seperated[0])
-    print('countries: ', seperated[1])
-    print('score: ', seperated[2])
-    print('------------------------')
+user_year = simpledialog.askstring('Year', 'type in a year')
+
+def find_game(year):
+    file = open('/Users/maxbotta/Desktop/Berlitz/DigitalSchool/python_course/do1745/module_12/uefa_euro.txt', 'r')
+    for line in file:
+        line = line.rstrip('\n')
+        seperated = line.split('/')
+        print('year: ', seperated[0])
+        print('countries: ', seperated[1])
+        print('score: ', seperated[2])
+        print('------------------------')
+        
+    return seperated[0], seperated[1]
+
+countries, score = find_game(user_year)
---
+++ python_course/do1745/module_8/recipes.py
@@ -0,0 +1,116 @@
+
+recipes = [
+    #asian cusisine
+    {
+        
+        "name": "Sushi",
+        "ingredients": ["rice", "seaweed", "fish", "soy sauce"],
+        "side_dishes": ["miso soup", "edamame"],
+        "spicy": False,
+        "meal_time": "dinner",
+        "country": "Japan",
+        "cuisine": "asian"
+    },
+    {
+        "name": "Pad Thai",
+        "ingredients": ["rice noodles", "shrimp", "peanuts", "egg"],
+        "side_dishes": ["spring rolls", "fried rice"],
+        "spicy": True,
+        "meal_time": "lunch",
+        "country": "Thailand",
+        "cuisine": "asian"
+    },
+    {
+        "name": "Bibimbap",
+        "ingredients": ["rice", "beef", "vegetables", "egg"],
+        "side_dishes": ["kimchi", "dumplings"],
+        "spicy": True,
+        "meal_time": "dinner",
+        "country": "Korea",
+        "cuisine": "asian"
+    },
+    #italian cuisine
+    {
+        "name": "Pizza",
+        "ingredients": ["dough", "tomato sauce", "cheese", "pepperoni"],
+        "side_dishes": ["salad", "breadsticks"],
+        "spicy": False,
+        "meal_time": "dinner",
+        "country": "Italy",
+        "cuisine": "italian"
+    },
+    {
+        "name": "Pasta Carbonara",
+        "ingredients": ["pasta", "bacon", "egg", "cheese"],
+        "side_dishes": ["bread", "salad"],
+        "spicy": False,
+        "meal_time": "lunch",
+        "country": "Italy",
+        "cuisine": "italian"
+    },
+    {
+        "name": "Lasagna",
+        "ingredients": ["pasta", "meat", "cheese", "tomato sauce"],
+        "side_dishes": ["bread", "salad"],
+        "spicy": False,
+        "meal_time": "dinner",
+        "country": "Italy",
+        "cuisine": "italian"
+    },
+    #greek cuisine
+    {
+        "name": "Gyros",
+        "ingredients": ["pita", "lamb", "tzatziki", "tomato"],
+        "side_dishes": ["fries", "salad"],
+        "spicy": False,
+        "meal_time": "lunch",
+        "country": "Greece",
+        "cuisine": "greek"
+    },
+    {
+        "name": "Moussaka",
+        "ingredients": ["eggplant", "meat", "bechamel", "tomato sauce"],
+        "side_dishes": ["bread", "salad"],
+        "spicy": False,
+        "meal_time": "dinner",
+        "country": "Greece",
+        "cuisine": "greek"
+    },
+    {
+        "name": "Dolma",
+        "ingredients": ["grape leaves", "rice", "meat", "lemon"],
+        "side_dishes": ["salad", "bread"],
+        "spicy": False,
+        "meal_time": "dinner",
+        "country": "Greece",
+        "cuisine": "greek"
+    },
+    #mexican cuisine
+    {
+        "name": "Tacos",
+        "ingredients": ["tortilla", "beef", "salsa", "guacamole"],
+        "side_dishes": ["rice", "beans"],
+        "spicy": True,
+        "meal_time": "lunch",
+        "country": "Mexico",
+        "cuisine": "mexican"
+    },
+    {
+        "name": "Enchiladas",
+        "ingredients": ["tortilla", "chicken", "cheese", "salsa"],
+        "side_dishes": ["rice", "beans"],
+        "spicy": True,
+        "meal_time": "dinner",
+        "country": "Mexico",
+        "cuisine": "mexican"
+    },
+    {
+        "name": "Chiles Rellenos",
+        "ingredients": ["chiles", "cheese", "meat", "sauce"],
+        "side_dishes": ["rice", "beans"],
+        "spicy": True,
+        "meal_time": "dinner",
+        "country": "Mexico",
+        "cuisine": "mexican"
+    }
+]
