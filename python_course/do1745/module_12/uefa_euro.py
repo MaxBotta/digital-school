bbb87c80171dc98a38c17e68e3e1d90dc2a92cee
Maximilian Botta <max.botta@gmx.de>
2024-11-28 19:54:00 +0100

    do1745 uefa and buttons

--- python_course/do1745/module_12/uefa_euro.py
+++ python_course/do1745/module_12/uefa_euro.py
@@ -2,20 +2,30 @@ from tkinter import Tk, simpledialog, messagebox
 
 root = Tk()
 
-messagebox.showinfo('Test', 'Hallo Welt')
+root.withdraw()
 
-user_year = simpledialog.askstring('Year', 'type in a year')
-
-def find_game(year):
+def find_game(input_year):
     file = open('/Users/maxbotta/Desktop/Berlitz/DigitalSchool/python_course/do1745/module_12/uefa_euro.txt', 'r')
     for line in file:
         line = line.rstrip('\n')
         seperated = line.split('/')
-        print('year: ', seperated[0])
-        print('countries: ', seperated[1])
-        print('score: ', seperated[2])
-        print('------------------------')
+        year = seperated[0]
+        country = seperated[1]
+        score = seperated[2]
         
-    return seperated[0], seperated[1]
+        if year == input_year:
+            file.close()
+            return year, country, score
+    
+    return None, None, None
 
-countries, score = find_game(user_year)
+def init_app():
+    user_year = simpledialog.askstring('Year', 'type in a year')
+    year, countries, score = find_game(user_year)
+    
+    if score == None:
+        messagebox.showinfo('Result', 'No result found for the year ' + user_year)
+    else:
+        messagebox.showinfo('Result', 'The result of the year ' + year + ' was ' + countries + ' with a score of ' + score)
+
+init_app()
