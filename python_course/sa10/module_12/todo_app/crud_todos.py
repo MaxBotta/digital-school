467ce42a74302ba50ab0c0572a65647d88c94335
Maximilian Botta <max.botta@gmx.de>
2024-12-14 11:35:55 +0100

    sa10 show todos, add todo

--- python_course/sa10/module_12/todo_app/crud_todos.py
+++ python_course/sa10/module_12/todo_app/crud_todos.py
@@ -2,18 +2,28 @@ import json
 
 path = "/Users/maxbotta/Desktop/Berlitz/DigitalSchool/python_course/sa10/module_12/todo_app/todos.json"
 
-def create(new_todo):
-    file = open(path, 'w')
-    data = json.load(file)
-    todos = data["todos"]
-    todos.append(new_todo)
-    file.write(json.dumps(data))
-    file.close()
-
 def read():
     with open(path, 'r') as file:
         data = json.load(file)
         return data["todos"]
+    
+def create(new_todo_text):
+    todos = read()
+    
+    with open(path, 'w') as file:
+        
+        new_todo = {
+            'title': new_todo_text, 
+            'completed': False
+        }
+        
+        todos.append(new_todo)
+        
+        data = {
+            "todos": todos
+        }
+        
+        json.dump(data, file)
 
 def update():
     pass
--- python_course/sa10/module_12/todo_app/todo_app.py
+++ python_course/sa10/module_12/todo_app/todo_app.py
@@ -1,10 +1,21 @@
-from tkinter import Tk, simpledialog, messagebox
-from crud_todos import create, read
+from tkinter import Tk, Label, Entry, Button, ttk
+from crud_todos import read, create
 
 root = Tk()
-root.withdraw()
+style = ttk.Style(root)
+style.theme_use('aqua')
 
-new_todo = simpledialog.askstring("New Todo", "Enter a new todo.")
-create(new_todo)
+root.geometry('400x600')
+
+todos = read()
+for todo in todos:
+    label = ttk.Label(root, text=todo['title'])
+    label.pack(pady=10)
+
+entry = ttk.Entry(root)
+entry.pack(pady=10)
+
+button = ttk.Button(root, text='Add task', command=lambda: create(entry.get()))
+button.pack(pady=10)
 
 root.mainloop()
