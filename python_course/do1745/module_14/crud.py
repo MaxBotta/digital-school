c0fa2bc9bac24eda224547ac04c2a950aea4f7d6
Maximilian Botta <max.botta@gmx.de>
2025-01-16 19:56:38 +0100

    do1745 added todos

--- python_course/do1745/module_14/crud.py
+++ python_course/do1745/module_14/crud.py
@@ -1,6 +1,23 @@
 import json
 
+path = "/Users/maxbotta/Desktop/Berlitz/DigitalSchool/python_course/do1745/module_14/todos.json"
+
 def read():
-    with open("todos.json") as file:
+    with open(path) as file:
         data = json.load(file)
         return data["todos"]
+    
+def delete(id):
+    todos = read()
+    
+    #finde das Todo mit der richtigen id und löschen es
+    for index, todo in enumerate(todos):
+        if todo['id'] is id:
+            todos.pop(index)
+            break
+        
+    with open(path, 'w') as file:
+        new_data = {
+            "todos": todos
+        }
+        json.dump(new_data, file)
--- python_course/do1745/module_14/todo_app.py
+++ python_course/do1745/module_14/todo_app.py
@@ -1,5 +1,5 @@
 import customtkinter as ctk
-from crud import read
+from crud import read, delete
 
 app = ctk.CTk()
 # app.geometry('400x600')
@@ -11,17 +11,6 @@ title.grid(row=0, column=0, padx=10, pady=10, sticky="w")
 todos_frame = ctk.CTkFrame(app)
 todos_frame.grid(row=1, column=0, padx=10, pady=10, sticky="w")
 
-#Todos in frame einfügen
-todo_1 = ctk.CTkLabel(todos_frame, text="Todo 1")
-todo_1.grid(row=0, column=0, padx=10, pady=5, sticky="w")
-delete_1 = ctk.CTkButton(todos_frame, text="Delete")
-delete_1.grid(row=0, column=1, padx=10, pady=5, sticky="e")
-delete_1_2 = ctk.CTkButton(todos_frame, text="Delete")
-delete_1_2.grid(row=0, column=2, padx=10, pady=5, sticky="e")
-
-
-todo_2 = ctk.CTkLabel(todos_frame, text="Todo 2")
-todo_2.grid(row=1, column=0, padx=10, pady=5, sticky="w")
 
 #Frame für Eingabe anlegen
 input_frame = ctk.CTkFrame(app)
@@ -33,6 +22,18 @@ entry.grid(row=0, column=0, padx=10, pady=10, sticky="w")
 
 add_button = ctk.CTkButton(input_frame, text="Add todo")
 add_button.grid(row=0, column=1, padx=10, pady=10, sticky="w")
+
+def create_todos():
+    todos = read()
+    for index, todo in enumerate(todos):
+        todo_label = ctk.CTkLabel(todos_frame, text=todo['task'])
+        todo_label.grid(row=index, column=0, padx=10, pady=5, sticky="w")
+        checkbox = ctk.CTkCheckBox(todos_frame, text="", width=22, height=22, fg_color="green")
+        checkbox.grid(row=index, column=1, padx=10, pady=5, sticky="e")
+        delete_button = ctk.CTkButton(todos_frame, text="X", width=22, height=22, command=lambda id=todo['id']: delete(id))
+        delete_button.grid(row=index, column=2, padx=10, pady=5, sticky="e")
+        
+create_todos()
 
 
 app.mainloop()
