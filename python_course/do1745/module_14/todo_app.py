fa4066ab312e6d515081cebedc140d01eae3eaa4
Maximilian Botta <max.botta@gmx.de>
2025-01-23 18:45:49 +0100

    do1745 delete todos

--- python_course/do1745/module_14/todo_app.py
+++ python_course/do1745/module_14/todo_app.py
@@ -1,3 +1,4 @@
+from venv import create
 import customtkinter as ctk
 from crud import read, delete
 
@@ -11,14 +12,12 @@ title.grid(row=0, column=0, padx=10, pady=10, sticky="w")
 todos_frame = ctk.CTkFrame(app)
 todos_frame.grid(row=1, column=0, padx=10, pady=10, sticky="w")
 
-
 #Frame für Eingabe anlegen
 input_frame = ctk.CTkFrame(app)
 input_frame.grid(row=2, column=0, padx=10, pady=10, sticky="w")
 
 entry = ctk.CTkEntry(input_frame)
 entry.grid(row=0, column=0, padx=10, pady=10, sticky="w")
-
 
 add_button = ctk.CTkButton(input_frame, text="Add todo")
 add_button.grid(row=0, column=1, padx=10, pady=10, sticky="w")
@@ -30,10 +29,20 @@ def create_todos():
         todo_label.grid(row=index, column=0, padx=10, pady=5, sticky="w")
         checkbox = ctk.CTkCheckBox(todos_frame, text="", width=22, height=22, fg_color="green")
         checkbox.grid(row=index, column=1, padx=10, pady=5, sticky="e")
-        delete_button = ctk.CTkButton(todos_frame, text="X", width=22, height=22, command=lambda id=todo['id']: delete(id))
+        delete_button = ctk.CTkButton(todos_frame, text="X", width=22, height=22, command=lambda id=todo['id']: delete_todo(id))
         delete_button.grid(row=index, column=2, padx=10, pady=5, sticky="e")
-        
-create_todos()
 
+def update_ui():
+    #delete all todos in ui
+    for widget in todos_frame.winfo_children():
+        widget.destroy()
+    #create todos again
+    create_todos()
+    
+def delete_todo(todo_id):
+    delete(todo_id)
+    update_ui()
+              
+create_todos()
 
 app.mainloop()
