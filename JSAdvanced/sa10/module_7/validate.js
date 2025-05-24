36ffc7f1c0597bb1a3926e34c96f599ff35442f0
MaxBotta <max.botta@gmx.de>
2025-05-24 10:58:24 +0200

    svg and how to animate

--- JSAdvanced/sa10/module_7/validate.js
+++ JSAdvanced/sa10/module_7/validate.js
@@ -1,16 +1,23 @@
 
 function validate() {
     const passEl = document.getElementById("password")
-    const value = passEl.value
+    const password = passEl.value
     const numbers = [0,1,2,3,4,5,6,7,8,9]
-
-    if(numbers.some(el => value.includes(el))) {
-        return true
-    }
-    
     const infoEl = document.getElementById("info")
-    infoEl.innerHTML = "Das Passwort muss eine Zahl beinhalten!"
-    passEl.classList.add("hint")
 
-    return false
+    // Das Passwort muss 8 Zeichen lang sein
+    if(password.length < 8) {
+        infoEl.innerHTML = "Das Passwort muss 8 Zeichen beinhalten!"
+        passEl.classList.add("hint")
+        return false
+    }
+
+    // Das Passwort muss eine Zahl beinhalten
+    if(numbers.some(el => password.includes(el)) === false) {
+        infoEl.innerHTML = "Das Passwort muss eine Zahl beinhalten!"
+        passEl.classList.add("hint")
+        return false
+    }
+
+    return true
 }
