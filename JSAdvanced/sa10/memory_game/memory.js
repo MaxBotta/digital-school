a3f03c85e9c1b5f67d0cd436afeac6478050d98e
MaxBotta <max.botta@gmx.de>
2025-09-27 11:39:13 +0200

    liste mit jeweils zwei karten

--- JSAdvanced/sa10/memory_game/memory.js
+++ JSAdvanced/sa10/memory_game/memory.js
@@ -1,4 +1,3 @@
-
 /* Todos
     - Erstelle map mit Karten in zufälliger Anordnung
     - Wenn eine Karte geklickt wird, Bild anzeigen
@@ -8,27 +7,58 @@
     - Spiel endet wenn alle Karten aufgedeckt wurden
 **/
 
-const game = document.querySelector('#game')
+const game = document.querySelector("#game");
+
+const fruits = [
+  "Acorn",
+  "Apple",
+  "Apple_Green",
+  "Banana",
+  "Cherry",
+  "Egg",
+  "Eggs",
+  "Green_Grape",
+  "Hearth",
+  "Hearth_Pink",
+  "Leaf_green",
+  "Leaf_Yellow",
+  "Lemon",
+  "Lime",
+  "Orange",
+  "Peanut",
+  "Peer",
+  "PineApple",
+  "Plum",
+  "Potato",
+  "Red_Grape",
+  "StrawBerry",
+  "Walnut",
+  "Watermelon",
+];
+
+const doubleFruits = [...fruits, ...fruits];
 
 function createMap(countX, countY) {
+  //Karten erstellen
+  for (let x = 0; x < countX; x++) {
+    const newCol = document.createElement("div");
+    newCol.className = "col";
+    game.appendChild(newCol);
 
-    //Karten erstellen
-    for(let x = 0; x < countX; x++) {
-        const newCol = document.createElement('div')
-        newCol.className = "col"
-        game.appendChild(newCol)
-        
-        for(let y = 0; y < countY; y++) {
-            const card = document.createElement('div')
-            card.className = "card"
-            // card.innerText = `x: ${x} y: ${y}`
-            const newImg = document.createElement('img')
-            newImg.src = './images/Acorn_96x96.png'
-            card.appendChild(newImg)
-            newCol.appendChild(card)
-        }
+    // Erstelle Karten in der Spalte
+    for (let y = 0; y < countY; y++) {
+      const card = document.createElement("div");
+      card.className = "card";
+      // card.innerText = `x: ${x} y: ${y}`
+
+      //Erstelle Bild
+      const source = doubleFruits.pop();
+      const newImg = document.createElement("img");
+      newImg.src = `./images/${source}_96x96.png`;
+      card.appendChild(newImg);
+      newCol.appendChild(card);
     }
+  }
 }
 
 createMap(6, 6);
-
