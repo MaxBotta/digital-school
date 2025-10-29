5bed8aba9a0c54c7603dc4bab14f835acac43d63
MaxBotta <max.botta@gmx.de>
2025-10-29 18:25:10 +0000

    Mi17:45 Todo app

---
+++ JSAdvanced/todo_app/functions.js
@@ -0,0 +1,44 @@
+// Referenzen Html Element
+const todosElement = document.querySelector("#todos");
+const inputElement = document.querySelector("#todo-input");
+
+// Liste aller Todos
+let todos = [];
+
+function addTodo() {
+  const value = inputElement.value;
+  todos.push(value);
+  renderTodos();
+}
+
+function removeTodo(value) {
+  todos = todos.filter((todo) => todo !== value);
+  renderTodos();
+}
+
+function renderTodos() {
+  // leere die ul liste im html
+  todosElement.innerHTML = "";
+
+  // Füge ein li für jedes Todo hinzu
+  for (let todo of todos) {
+    const liElmnt = document.createElement("li");
+    liElmnt.innerText = todo;
+
+    //Datum
+    const dateElmnt = document.createElement("div");
+    const currentDate = new Date();
+    dateElmnt.classList.add("date");
+    dateElmnt.innerText = currentDate.toLocaleDateString();
+    liElmnt.appendChild(dateElmnt);
+
+    // Erstelle löschen Button
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("delete");
+    deleteBtn.innerText = "Entfernen";
+    deleteBtn.onclick = () => removeTodo(todo);
+
+    liElmnt.appendChild(deleteBtn);
+    todosElement.appendChild(liElmnt);
+  }
+}
