01991d2e6adafeddfe615bb0ee4c679647b171b1
MaxBotta <max.botta@gmx.de>
2026-02-14 10:35:11 +0100

    fetch art data

--- JSAdvanced/sa10/module_36/script.js
+++ JSAdvanced/sa10/module_36/script.js
@@ -18,10 +18,22 @@ function renderList(data) {
   list.innerHTML = "";
   for (let item of data) {
     const newLi = document.createElement("li");
-    newLi.innerText = item.title;
-    const img = document.createElement('img')
-    img.src = `https://www.artic.edu/iiif/2/${item.image_id}/full/843,/0/default.jpg`
-    newLi.appendChild(img)
+    newLi.classList.add("row");
+
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("card");
+    newDiv.classList.add("mb-4");
+    newDiv.classList.add("col-4");
+
+    const img = document.createElement("img");
+    img.src = `https://www.artic.edu/iiif/2/${item.image_id}/full/843,/0/default.jpg`;
+
+    const newP = document.createElement("p");
+    newP.innerText = item.title;
+
+    newDiv.appendChild(img);
+    newDiv.appendChild(newP);
+    newLi.appendChild(newDiv);
     list.appendChild(newLi);
   }
 }
